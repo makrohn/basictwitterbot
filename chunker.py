ace96cf55e96125b55499fbabc9dec44f4e940d8
Matthew Krohn <mkrohn@hubspot.com>
2017-11-10 08:10:57 -0500

    Keep things threaded

--- chunker.py
+++ chunker.py
@@ -8,18 +8,20 @@ TEXT=open("federalist.txt","r")
 
 CHUNKED_TEXT = TEXT.read().split("\n\n")
 
+HANDLE = "@TweetFedPapers"
+
 def create_chunks():
     for chunk in CHUNKED_TEXT:
         chunk = chunk.replace("\n", " ")
         remainder = len(chunk)
         while remainder > 0:
-            if len(chunk) > 140:
-                knuhc = chunk[:139]
+            if len(chunk) > 120:
+                knuhc = chunk[:119]
                 knuhc = knuhc[::-1]
                 knuhc = knuhc.split(" ",1)
                 tweet_length = len(knuhc[0])
-                tweet = chunk[:139-tweet_length]
-                chunk = chunk[139-tweet_length:]
+                tweet = chunk[:119-tweet_length] + " " + HANDLE
+                chunk = chunk[119-tweet_length:]
                 remainder = len(chunk)
                 yield(tweet)
             else:
@@ -29,18 +31,22 @@ def create_chunks():
 # for tweet in create_chunks():
 #     print(tweet)
 
-def tweet(text):
+def tweet(text, replyto):
     base_url = "https://api.twitter.com/1.1/statuses/update.json"
     payload = {
         "status": text,
         "display_coordinates": "false",
+        "in_reply_to_status_id": replyto
         }
     auth = OAuth1('MOFzgeRyg9KHhIRNpX90Sqefu', 'tdqULHQK0bXSF7zeCJMm0ICUmGMgc8VoKzWmDxvgTa5A5Ay3JY',
         '882755550047608832-QBJTXZVkSzB6InirdAED5GATZ0UCSK2', 'M4dUNodScnd7p9e8kMR93VZfG2mgWtw2a7ecUiMn590zN')
     result = requests.post(base_url, auth=auth, params=payload)
-    print(result.json())
+    return result.json()
+
+inreply = ""
 
 for update in create_chunks():
-    tweet(update)
+    answer = tweet(update, inreply)
+    inreply = answer["id"]
     time.sleep(60)
     
